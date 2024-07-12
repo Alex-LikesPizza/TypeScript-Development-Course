3a70656f38383fd27b8ad8df00b09fc624d9b417
Alex-LikesPizza <alexandru13321@gmail.com>
2024-07-12 23:34:17 +0300

    linkedList advance

--- #Algorithms-and-Data-Structures/Data Structures/LinkedList.ts
+++ #Algorithms-and-Data-Structures/Data Structures/LinkedList.ts
@@ -1,21 +1,67 @@
-class LinkedListNode<T> {
-  value: T;
-  nextNode: LinkedListNode<T>;
+interface LinkedListNode{
+  value: any,
+  next: LinkedListNode | null,
+}
 
-  constructor(val: T, next: LinkedListNode<T>){
-    this.value = val;
-    this.nextNode = next;
+// let linkedList : {head: LinkedListNode} = {
+//   head: {
+//     value: 10,
+//     next: {
+//       value: 5,
+//       next: {
+//         value: 16,
+//         next: null
+//       }
+//     }
+//   }
+// }
+class ListNode implements LinkedListNode {
+  value: any;
+  next: LinkedListNode | null;
+
+  constructor(value: any){
+    this.value = value;
+    this.next = null
   }
+}
+class LinkedList {
+  head: LinkedListNode
+  tail: LinkedListNode
+  length: number
 
-  deleteNext(){
-    this.nextNode = this.nextNode.nextNode;
+  constructor(value : any){
+    this.head = new ListNode(value);
+    this.tail = this.head;
+    this.length = 1;
+  }
+  append(value: any){
+    const prevTail = this.tail;
+    this.tail = new ListNode(value);
+    prevTail.next = this.tail;
+    this.length++;
+    return this;
+  }
+  prepend(value: any){
+    const prevHead = this.head;
+    this.head = new ListNode(value);
+    this.head.next = prevHead;
+    this.length++;
+    return this;
+  }
+  insert(index: number, value: any){
+    const newNode = new ListNode(value);
+    let currentNode = this.head;
+    for(let i = 0; i < index - 1; i++){
+      currentNode = currentNode.next
+    }
   }
 }
 
-class LinkedList<T> {
-  list: LinkedListNode<T>[];
+const list = new LinkedList(10);
 
-  pushBack(value: T){
-    
-  }
-}
+list.append(5);
+list.append(16);
+list.prepend(1);
+list.insert(2, 2);
+
+console.log(list);
