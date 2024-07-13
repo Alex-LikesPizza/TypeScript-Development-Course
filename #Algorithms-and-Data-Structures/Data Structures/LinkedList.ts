bfcfc78d2109717a0c3faa7ade9e192828d72b38
Alex-LikesPizza <alexandru13321@gmail.com>
2024-07-13 20:35:05 +0300

    linkedLists finish

--- #Algorithms-and-Data-Structures/Data Structures/LinkedList.ts
+++ #Algorithms-and-Data-Structures/Data Structures/LinkedList.ts
@@ -1,3 +1,15 @@
+/*
+Pros:
+  Fast Insertions
+  Fast Deletions
+  Ordered
+  Flexible Size
+Cons:
+  Slow Lookup
+  More Memory
+*/
+
+
 interface LinkedListNode{
   value: any,
   next: LinkedListNode | null,
@@ -34,6 +46,16 @@ class LinkedList {
     this.tail = this.head;
     this.length = 1;
   }
+  at(index: number){
+    let currentNode = this.head;
+    for(let i = 0; i < index; i++){
+      if(!currentNode.next){
+        return this.tail;
+      }
+      currentNode = currentNode.next;
+    }
+    return currentNode;
+  }
   append(value: any){
     const prevTail = this.tail;
     this.tail = new ListNode(value);
@@ -50,18 +72,66 @@ class LinkedList {
   }
   insert(index: number, value: any){
     const newNode = new ListNode(value);
-    let currentNode = this.head;
-    for(let i = 0; i < index - 1; i++){
-      currentNode = currentNode.next
+    if(index === 0)
+      return this.prepend(value);
+
+    let prevNode = this.at(index - 1);
+    const nextNode = prevNode.next;
+    prevNode.next = newNode;
+    newNode.next = nextNode;
+    
+    this.length++;
+    return this;
+  }
+  unshift(){
+    const prevFirst = JSON.parse(JSON.stringify(this.head));
+    if(this.head.next) this.head = this.head.next;
+    else this.head.value = null;
+    this.length--;
+    return prevFirst; 
+  }
+  pop(){
+    if(this.length === 0) return null;
+    const newLastNode = this.at(this.length - 2);
+    const lastNode = this.at(this.length - 1);
+    if(newLastNode)
+      newLastNode.next = null;
+    this.length--;
+    return lastNode;
+  }
+  delete(index: number){
+    if(index === 0){
+      return this.unshift();
     }
+    this.length--;
+    const prevDeleteNode = this.at(index - 1);
+    const nextDeleteNode = prevDeleteNode.next?.next;
+    prevDeleteNode.next = nextDeleteNode? nextDeleteNode : null;
+  }
+  reverse() {
+    let currentNode: LinkedListNode | null = this.head;
+    let previousNode: LinkedListNode | null = null;
+    let nextNode: LinkedListNode | null = null;
+  
+    while (currentNode !== null) {
+      nextNode = currentNode.next;
+      currentNode.next = previousNode;
+      previousNode = currentNode;
+      currentNode = nextNode;
+    }
+  
+    const newTail = this.head;
+    this.head = this.tail;
+    this.tail = newTail;
+    this.tail.next = null;
   }
 }
 
-const list = new LinkedList(10);
+const list = new LinkedList(1);
 
-list.append(5);
-list.append(16);
-list.prepend(1);
-list.insert(2, 2);
+list.append(2);
+list.append(3);
+list.append(4);
+list.reverse();
 
 console.log(list);
